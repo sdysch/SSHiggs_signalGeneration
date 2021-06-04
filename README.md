d1c6c8b407164332cabe29884762633cd04d2fc0
Samuel Dezso Dysch <sdysch94@gmail.com>
2021-06-04 09:06:09 +0100

    Local setup

--- README.md
+++ README.md
@@ -1,14 +1,9 @@
 # SSHiggs_signalGeneration
 
-# For first setup
-
+# First time setup
+- We need to add the model to the MadGraph models directory
 ```
-setupATLAS
-lsetup git
-asetup AthGeneration,21.6.48
+cp -r /path/to/MLRSM /path/to/MadGraph/MG5_aMC_v3_1_1/models/
 ```
 
-# On every login
-```
-source setup.sh
-```
+- It should be possible to add the model to `PYTHONPATH` (see [setup.sh](setup.sh)]), but I could not get this to work yet
--- setup.sh
+++ setup.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
-command -v setupATLAS > /dev/null || ( export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase \
-&& alias setupATLAS='source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh')
-setupATLAS
-
-lsetup git
-asetup --restore
-
 # path to Madgraph model
-MODELPATH="/afs/cern.ch/user/s/sdysch/private/MadGraph_models/MLRSM/MLRSM_1-1-5/"
-export PYTHONPATH=$MODELPATH
-
+MODELPATH="/home/sdysch/Documents/Manchester/PostDoc/MG_models"
+export PYTHONPATH=$MODELPATH:$PYTHONPATH
 unset MODELPATH
