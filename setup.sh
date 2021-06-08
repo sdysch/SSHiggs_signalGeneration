c44e93ecefb50c69dd4ca8ec459f62f2baf03d16
Samuel Dezso Dysch <sdysch94@gmail.com>
2021-06-08 14:28:00 +0100

    Add path to LHAPDF in setup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 # path to Madgraph model
-MODELPATH="/home/sdysch/Documents/Manchester/PostDoc/MG_models"
-export PYTHONPATH=$MODELPATH:$PYTHONPATH
-unset MODELPATH
+#MODELPATH="/home/sdysch/Documents/Manchester/PostDoc/MG_models"
+#export PYTHONPATH=$MODELPATH:$PYTHONPATH
+#unset MODELPATH
+LHAPDF="/home/sdysch/Documents/Manchester/PostDoc/MadGraphInstall/MG5_aMC_v3_1_1/HEPTools/lhapdf6_py3/lib"
+export PYTHONPATH=$LHAPDF:$PYTHONPATH
+unset LHAPDF
