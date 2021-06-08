93c90f84f66f88c61bfc4883346296d9cd121804
Samuel Dezso Dysch <sdysch94@gmail.com>
2021-06-08 14:56:34 +0100

    Setting other required environment variables

--- setup.sh
+++ setup.sh
@@ -3,6 +3,13 @@
 #MODELPATH="/home/sdysch/Documents/Manchester/PostDoc/MG_models"
 #export PYTHONPATH=$MODELPATH:$PYTHONPATH
 #unset MODELPATH
-LHAPDF="/home/sdysch/Documents/Manchester/PostDoc/MadGraphInstall/MG5_aMC_v3_1_1/HEPTools/lhapdf6_py3/lib"
+
+BASEPATH="/home/sdysch/Documents/Manchester/PostDoc/MadGraphInstall/MG5_aMC_v3_1_1/"
+
+# LHAPDF
+LHAPDF="${BASEPATH}/HEPTools/lhapdf6_py3/lib"
 export PYTHONPATH=$LHAPDF:$PYTHONPATH
+export LD_LIBRARY_PATH="${BASEPATH}/HEPTools/lhapdf6_py3/lib"
+
 unset LHAPDF
+unset BASEPATH
