41619f31c5ff9a18e04848531f36380f7f2c4203
Samuel Dysch <samuel.dysch@cern.ch>
2021-06-17 12:40:01 +0200

    setup script for lxplus

--- setup.sh
+++ setup.sh
@@ -4,12 +4,19 @@
 #export PYTHONPATH=$MODELPATH:$PYTHONPATH
 #unset MODELPATH
 
-BASEPATH="/home/sdysch/Documents/Manchester/PostDoc/MadGraphInstall/MG5_aMC_v3_1_1/"
+if grep -q "$HOSTNAM" <<< "lxplus"; then
+	alias runMG="/afs/cern.ch/work/s/sdysch/private/SSHiggs/MadGraphInstall/MG5_aMC_v3_1_1/bin/mg5_aMC"
+	setupATLAS
+	lsetup "python 3.7"
+	lsetup git
+else
+	BASEPATH="/home/sdysch/Documents/Manchester/PostDoc/MadGraphInstall/MG5_aMC_v3_1_1/"
 
-# LHAPDF
-LHAPDF="${BASEPATH}/HEPTools/lhapdf6_py3/lib"
-export PYTHONPATH=$LHAPDF:$PYTHONPATH
-export LD_LIBRARY_PATH="${BASEPATH}/HEPTools/lhapdf6_py3/lib"
+	# LHAPDF
+	LHAPDF="${BASEPATH}/HEPTools/lhapdf6_py3/lib"
+	export PYTHONPATH=$LHAPDF:$PYTHONPATH
+	export LD_LIBRARY_PATH="${BASEPATH}/HEPTools/lhapdf6_py3/lib"
 
-unset LHAPDF
-unset BASEPATH
+	unset LHAPDF
+	unset BASEPATH
+fi
