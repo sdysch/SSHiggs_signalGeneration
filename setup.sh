e69d9e8f6c43230308b729d101ef8b6190f8bb3d
Samuel Dezso Dysch <sdysch94@gmail.com>
2021-06-21 14:19:23 +0100

    Locally

--- setup.sh
+++ setup.sh
@@ -4,19 +4,18 @@
 #export PYTHONPATH=$MODELPATH:$PYTHONPATH
 #unset MODELPATH
 
-if grep -q "$HOSTNAM" <<< "lxplus"; then
-	alias runMG="/afs/cern.ch/work/s/sdysch/private/SSHiggs/MadGraphInstall/MG5_aMC_v3_1_1/bin/mg5_aMC"
-	setupATLAS
-	lsetup "python 3.7"
-	lsetup git
-else
-	BASEPATH="/home/sdysch/Documents/Manchester/PostDoc/MadGraphInstall/MG5_aMC_v3_1_1/"
+#if grep -q "$HOSTNAME" "lxplus"; then
+	#alias runMG="/afs/cern.ch/work/s/sdysch/private/SSHiggs/MadGraphInstall/MG5_aMC_v3_1_1/bin/mg5_aMC"
+	#setupATLAS
+	#lsetup "python 3.7"
+	#lsetup git
+#else
+BASEPATH="/home/sdysch/Documents/Manchester/PostDoc/MadGraphInstall/MG5_aMC_v3_1_1/"
 
-	# LHAPDF
-	LHAPDF="${BASEPATH}/HEPTools/lhapdf6_py3/lib"
-	export PYTHONPATH=$LHAPDF:$PYTHONPATH
-	export LD_LIBRARY_PATH="${BASEPATH}/HEPTools/lhapdf6_py3/lib"
+# LHAPDF
+LHAPDF="${BASEPATH}/HEPTools/lhapdf6_py3/lib"
+export PYTHONPATH=$LHAPDF:$PYTHONPATH
+export LD_LIBRARY_PATH="${BASEPATH}/HEPTools/lhapdf6_py3/lib"
 
-	unset LHAPDF
-	unset BASEPATH
-fi
+unset LHAPDF
+unset BASEPATH
